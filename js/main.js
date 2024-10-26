58c631783e51ecef92d773fac35575b41c4ef815
Андрей Рыжко <ryzhkoandrey@gmail.com>
2024-10-26 15:15:01 +0300

    header (js)

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
 // CUSTOM SELECT
 
 customSelect('select');
+
+// EASE PICK
+
+const picker = new easepick.create({
+    element: document.getElementById('date'),
+    css: ['https://cdn.jsdelivr.net/npm/@easepick/bundle@1.2.1/dist/index.css'],
+});
