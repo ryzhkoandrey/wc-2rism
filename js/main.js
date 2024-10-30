05ae8d5d4d20e0e005b17594a9555283719155ed
Андрей Рыжко <ryzhkoandrey@gmail.com>
2024-10-30 17:31:09 +0200

    footer

--- js/main.js
+++ js/main.js
@@ -9,3 +9,16 @@ const picker = new easepick.create({
     css: ['https://cdn.jsdelivr.net/npm/@easepick/bundle@1.2.1/dist/index.css'],
     format: 'DD.MM.YY',
 });
+
+// SUBSCRIBE FORM
+
+const inputSubscribe = document.querySelector('.subscribe__input');
+const label = document.querySelector('.subscribe__label');
+
+inputSubscribe.addEventListener('input', () => {
+    if (inputSubscribe.value.trim() !== '') {
+        label.classList.add('subscribe__label--top');
+    } else {
+        label.classList.remove('subscribe__label--top');
+    }
+});
