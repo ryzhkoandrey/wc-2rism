3458fd27f349b6c0c395a862cc6f799c22979d7a
Андрей Рыжко <ryzhkoandrey@gmail.com>
2024-10-31 14:07:12 +0200

    backup

--- js/main.js
+++ js/main.js
@@ -1,3 +1,24 @@
+// MOBILE NAV
+
+const navBtn = document.querySelector('.mobile-nav-btn');
+const nav = document.querySelector('.mobile-nav');
+const menuIcon = document.querySelector('.nav-icon');
+const fade = document.querySelector('.mobile-nav-fade');
+
+navBtn.onclick = function () {
+    nav.classList.toggle('mobile-nav--open');
+    fade.classList.toggle('mobile-nav-fade--open');
+    menuIcon.classList.toggle('nav-icon--active');
+    document.body.classList.toggle('no-scroll');
+};
+
+fade.onclick = function () {
+    nav.classList.toggle('mobile-nav--open');
+    fade.classList.toggle('mobile-nav-fade--open');
+    menuIcon.classList.toggle('nav-icon--active');
+    document.body.classList.toggle('no-scroll');
+};
+
 // CUSTOM SELECT
 
 customSelect('select');
@@ -10,7 +31,7 @@ const picker = new easepick.create({
     format: 'DD.MM.YY',
 });
 
-// SUBSCRIBE FORM
+// SUBSCRIBE FORM IN FOOTER
 
 const inputSubscribe = document.querySelector('.subscribe__input');
 const label = document.querySelector('.subscribe__label');
